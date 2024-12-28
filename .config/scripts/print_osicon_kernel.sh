a1b66054cddf6200ebde37f0af7ad480ffc21a19
Mathias Lorenz <lorenz.mathias@gmail.com>
2024-12-28 11:35:04 +0100

    Add custom script for printing kernel version and logo, the script can't be executed from the bar and the bar isn't showing on startup still :(

---
+++ .config/scripts/print_osicon_kernel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+OS_NAME="$(rg '^NAME=' /etc/os-release)"
+
+if [[ $OS_NAME == *"Arch"* ]]; then
+  OS_ICON=" "
+elif [[ $OS_NAME == *"Manjaro"* ]]; then
+  OS_ICON=" "
+fi
+
+if [[ $(uname -r) == *"lts"* ]]; then
+  LTS=" LTS"
+else
+  LTS=""
+fi
+
+# TODO fix showing if LTS and arch / manjaro
+[[ $(uname -r) =~ ([0-9]+\.[0-9]+\.[0-9]+) ]]
+
+echo "$OS_ICON ${BASH_REMATCH[1]}$LTS"
