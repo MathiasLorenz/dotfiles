c78e36bdef03f310da69a40ea5463a3463a90ae8
Mathias Lorenz <lorenz.mathias@gmail.com>
2025-01-11 18:04:58 +0100

    Use proper rofi-wayland package

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -109,7 +109,7 @@
     lazygit
 
     wlr-randr
-    rofi
+    rofi-wayland
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
