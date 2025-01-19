34e70cf86da876a2adb16e9aea832c8d1dcb83ad
MathiasLorenz <lorenz.mathias@gmail.com>
2025-01-19 21:34:23 +0100

    Comment about how to use 'raw' nixGL wrapper if you don't want home-manager to also manage the installation of gpu programs

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -139,6 +139,10 @@
     wlr-randr
     rofi-wayland
 
+    # Use this to install programs needing graphics card / opengl by spawning them with
+    # the nixGL wrapper
+    # (config.lib.nixGL.wrapOffload pkgs.qutebrowser)
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
