90d4c4a1f06d9c569ea2d27980e0b970a3183d44
MathiasLorenz <lorenz.mathias@gmail.com>
2025-01-28 08:45:52 +0100

    Add nyxt browser with home-manager

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -146,6 +146,9 @@
     # the nixGL wrapper
     # (config.lib.nixGL.wrapOffload pkgs.qutebrowser)
 
+    # nyxt browser
+    (config.lib.nixGL.wrapOffload pkgs.nyxt)
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
