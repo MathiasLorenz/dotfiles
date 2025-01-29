eaffb7f814774573bd90c7b5e2ca21d87da17a3d
MathiasLorenz <lorenz.mathias@gmail.com>
2025-01-29 10:15:04 +0100

    Play a bit with python lsp - pylsp seem to work ok, though it does not set the root directory correctly.........

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,7 +4,9 @@
 
 -- vim.g.lazyvim_python_lsp = "ruff"
 -- vim.g.lazyvim_python_lsp = "basedpyright"
-vim.g.lazyvim_python_lsp = "pyright"
+-- vim.g.lazyvim_python_lsp = "pyright"
+-- vim.g.lazyvim_python_lsp = "jedi_language_server"
+vim.g.lazyvim_python_lsp = "pylsp"
 
 -- Set root to always be cwd, otherwise the LSP might change it on the fly
 vim.g.root_spec = { "cwd" }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,6 +5,14 @@ return {
       servers = {
         ruff = {
           mason = true,
+          autostart = false,
+        },
+        jedi_language_server = {
+          mason = true,
+          autostart = false,
+        },
+        pylsp = {
+          mason = true,
         },
       },
     },
