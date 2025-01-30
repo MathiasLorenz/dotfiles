ba73fac1af1db19880f8111db62c7f86e310628c
MathiasLorenz <lorenz.mathias@gmail.com>
2025-01-30 11:39:54 +0100

    Play around with colorschemes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,14 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
+  -- { "slugbyte/lackluster.nvim" },
+  --  See https://github.com/zenbones-theme/zenbones.nvim?tab=readme-ov-file for colorschemes for zenbones
+  -- { "zenbones-theme/zenbones.nvim", dependencies = "rktjmp/lush.nvim" },
 
-  -- Configure LazyVim to load grubbox
+  -- Configure LazyVim to load colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "gruvbox",
     },
   },
 }
