8fbcb241020b5023d22725689d665cdaa873f8ff
MathiasLorenz <lorenz.mathias@gmail.com>
2025-02-09 12:36:09 +0100

    Update lazyvim and use snacks-picker and snacks-explorer instead of neo-tree and fzf-lua

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -1,6 +1,7 @@
 return {
   {
     "ibhagwan/fzf-lua",
+    enabled = false,
     opts = {
       defaults = {
         hidden = true,
---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {},
+      picker = {
+        sources = {
+          files = { hidden = true, ignored = true },
+          grep = { hidden = true, ignored = false },
+          explorer = { hidden = true, ignored = true },
+        },
+      },
+    },
+  },
+}
