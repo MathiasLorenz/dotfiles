095842042894c04ed279197692a346d146e8c310
MathiasLorenz <lorenz.mathias@gmail.com>
2025-02-18 16:09:59 +0100

    Remove asdf setup from fish setup

--- .config/home-manager/modules/fish.nix
+++ .config/home-manager/modules/fish.nix
@@ -65,22 +65,6 @@
       # Is not yet working, maybe just use home-manager sessionVariables...
       # fish_add_path ~/roc_nightly-linux_x86_64-2025-01-07-a089cf2
 
-      ### ASDF version manager
-      # ASDF configuration code
-      if test -z $ASDF_DATA_DIR
-          set _asdf_shims "$HOME/.asdf/shims"
-      else
-          set _asdf_shims "$ASDF_DATA_DIR/shims"
-      end
-
-      # Do not use fish_add_path (added in Fish 3.2) because it
-      # potentially changes the order of items in PATH
-      if not contains $_asdf_shims $PATH
-          set -gx --prepend PATH $_asdf_shims
-      end
-      set --erase _asdf_shims
-
-
       nitch
     '';
 
